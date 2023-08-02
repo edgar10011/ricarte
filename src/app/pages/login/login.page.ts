66489b90da897da600b1f053a390f910352e9a04
echoes1205 <leonardo.ochoaravelo@gmail.com>
2023-08-02 08:47:10 -0600

    registro funcionando

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -18,26 +18,33 @@ export class LoginPage {
   constructor(private router: Router, private http: HttpClient) { }
 
   authenticate() {
+    if (!this.username || !this.password) {
+      console.log('Ingresa un nombre de usuario y contraseña válidos.');
+      // Agregar lógica para mostrar un mensaje de error al usuario, por ejemplo, con un Toast o un mensaje en la interfaz de usuario.
+      return;
+    }
+
     console.log('Cargando inicio');
     const data = {
       username: this.username,
       password: this.password,
     };
 
-    this.http.post<LoginResponse>('http://localhost:3004/authenticate', data).subscribe(
+    this.http.post('http://localhost:3004/authenticate', data).subscribe(
       (response) => {
         console.log('Respuesta del servidor: ', response);
-        if (response.success) {
+        if (response) {
           console.log('Inicio de sesión exitoso');
-          this.router.navigate(['/plantas']); // Reemplaza 'plantas' con la ruta correcta para la página de plantas
+          this.router.navigate(['../plantas']); // Reemplaza 'plantas' con la ruta correcta para la página de plantas
         } else {
           console.log('Credenciales incorrectas');
+          // Agregar lógica para mostrar un mensaje de error al usuario, por ejemplo, con un Toast o un mensaje en la interfaz de usuario.
         }
       },
       (error) => {
         console.error('Error en la solicitud:', error);
         console.log('Hubo un error durante el inicio de sesión.');
-        // Agregar lógica para mostrar mensaje de error al usuario, por ejemplo, con un Toast o un mensaje en la interfaz de usuario.
+        // Agregar lógica para mostrar un mensaje de error al usuario, por ejemplo, con un Toast o un mensaje en la interfaz de usuario.
       }
     );
   }
